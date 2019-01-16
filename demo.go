fed57fb3ff10b3ffa8b93f5f26fff823dfabaca3
wangzhaozhou <zhaozhou.wang@roadshare.com>
2019-01-16 11:00:30 +0800

    test commit

--- demo.go
+++ demo.go
@@ -4,9 +4,14 @@ import "fmt"
 
 var a = 'a'
 var b = "a"
-var c bool
+var c bool = true
+var d = 123
+var e = 321
 
 func main() {
+
+	var f = d + e
+
 	//第一个go程序
-	fmt.Println(a, b, c)
+	fmt.Println(a, b, c, f)
 }
